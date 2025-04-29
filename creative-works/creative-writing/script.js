0f5d779047a971f9af14cd4d33c9a569af60b543
jessicadavies2003 <154709968+jessicadavies2003@users.noreply.github.com>
2025-04-29 17:57:21 +0100

    Version 4.2.1 Updates

--- creative-works/creative-writing/script.js
+++ creative-works/creative-writing/script.js
@@ -8,26 +8,36 @@ Detective Brighton (female) is told to take on the case, with her good friend Ja
 Will Brighton finally find out who the culprit of this disturbing case really is? Or will she be stuck in a wormhole with no way out?
         `,
         genre: "Murder Mystery",
+        bg: "linear-gradient(90deg,rgba(0, 0, 0, 1) 0%, rgba(117, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 100%)",
         teasers: {
             teaser1: {
-                teaserTitle: "Teaser 1 - Chapter 1 Draft Preview",
-                teaserDate: "24th February 2025",
-                teaserText: `"Detective!" Jamie called to Detective Brighton from her office door. Brighton was finishing up her report on a previous case report.<br><br>
-Startled, Brighton shot her head up to look at Jamie, with a confused expression. "Yes?"<br><br>
-"I need to speak with you in my office, now." Jamie spoke in a stern tone of voice, leaning on the door frame with her arms crossed. Brighton let out a huff in annoyance, and stood up from her seat, tucking her chair in neatly.<br><br>
-She made her way towards Jamie's office, as Jamie entered the office and took her seat. Brighton followed shortly after, entering the office with a small smile, and taking a seat right opposite Jamie's office chair. "You called, boss?"<br><br>
-"You got a new case." Jamie spoke in slight monotone, handing Brighton a yellow post it note with something written on it. "Here's the address."<br><br>
-The woman took the paper and looked at it briefly, then looked back up at Jamie with a confused look.<br><br>
-"I thought I had a case report to be completed by tomorrow morning. What do I do about that?"<br><br>
-Jamie let out a sigh, leaning back on her chair slightly and crossing her legs. "How much have you written for it currently?"<br><br>
-"4 pages. It's almost complete, so I could quickly fini-"<br><br>
-"No, just put it on my desk and I'll find someone else who isn't too busy to finish it off. New case means you drop everything and focus on it, regardless."<br><br>
-"But boss-"<br><br>
-"You heard me, Brighton. Get investigating, now."<br><br>
-Brighton let out a sigh. "Alright." She spoke, with a hint of annoyance in her tone of voice. She stood up from the seat, tucking in the chair, nodded at Jamie, before exiting her office and closing the door behind her.<br><br>
-She made her way to her desk to grab her jacket. While doing so, she looked over to James' office to see if he was there. No sign of him, so he must have been called earlier about this new case, or working on a different one. Who knows?<br><br>
-She confidently walked down the spiral staircase, and straight to the front door. Once outside, she took a moment to breathe in the fresh air, before heading to her car.
-                `
+                teaserTitle: "Teaser 1 - Chapter 1 Preview",
+                teaserDate: "29th April 2025",
+                teaserText: `<i>Finally, it's been too long of a day at work today, and I can finally head home for the weekend. Had about 10 calls in the office, and was constantly either working on reports, or investigating cases ranging from thievery to murder. Don't forget the number of times I've been called to my boss' office for late night calls. It's a pain for detectives like me to be working late, just because most crimes are committed late at night, then someone just so happens to be around that area at the time to immediately dial 999.</i><br><br>
+<i>Don't get me wrong, solving crimes can be satisfying once the true killer is revealed, and I do love my job occasionally. My boss can get really strict sometimes.</i><br><br>
+<i>However, it's strange to admit, but having James and Milton at work today was quite nice, having them keep the mood light and joyful. They keep me sane throughout the chaos, and it's nice to have that.</i><br><br>
+<i>Wait a moment... Usually my boss calls me around this time. No matter what time in the night a body is discovered, I still have to be awake and ready to discover it. Says the reason for this is so they can handle as many calls as possible. Yes it makes sense, so the person who was killed gets the justice they deserve, but all it does is strain us o-</i><br><br>
+<b>*Ring ring*</b><br><br>
+<b>*Ring ring*</b><br><br>
+<i>Yep, there it is.</i><br><br>
+Brighton looked down at her car's screen for a small moment, and let out a huff in annoyance seeing who it was, moving her eyes back onto the road ahead.<br><br>
+<b>Jamie (Boss) is calling</b><br><br>
+On the steering wheel were multiple buttons that performed various actions. One of which, on the left-hand side, was a phone icon, indicating the answer button. She tapped it, started off with a simple "Hello?", and immediately heard her boss from the other size, sounding rather irritated.<br><br>
+"Detective!" Jamie shot the word down the phone.<br><br>
+Trying not to sound too annoyed by her boss, she replied with a simple "Yes?"<br><br>
+"You got a new case." Jamie spoke in slight monotone.<br><br>
+"Already?" Brighton asked, startled. "Didn't I already have a case report to be finished by tomorrow?"<br><br>
+"You did." Jamie nodded from her side. "However, new case means you drop everything and focus on it, regardless. Especially this new case. It's a special one."<br><br>
+"Special?" Brighton tilted her head.<br><br>
+"You'll see when you know about the victim. Now drop that previous report."<br><br>
+"Boss-"<br><br>
+Jamie stood up and slammed her hands on the desk, eyeing the phone.<br><br>
+"You heard me, Brighton. Get to that address and start investigating. Now."<br><br>
+Brighton let out a sigh. "Alright." She spoke, with a hint of annoyance in her tone of voice. She stood up from the seat, tucking in the chair, nodded at Jamie, before hanging up the phone and letting out a sigh.<br><br>
+<i>Now you can understand the kind of boss I have to deal with.</i><br><br>
+She typed it into her smart car's maps application, got the directions ready and tapped "Start". She started the vehicle's engine, and immediately heard the female navigation's text to speech voice:<br><br>
+"In 300 yards, turn left onto the main road, then continue straight for 2 miles."<br><br>
+Brighton smiled, and made her way to the address.`
             },
             teaser2: {
                 teaserTitle: "Promotional Material #1",
@@ -42,6 +52,7 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
 
 document.querySelector("title").textContent = `${stories[id]['title']} - Jessica Davies`;
+document.body.style.background = stories[id]['bg'];
 
 document.getElementById('bookTitle').textContent = stories[id]['title'];
 document.getElementById('coverImg').src = stories[id]['img'];
@@ -61,7 +72,7 @@ for (let i = 0; i < len; i++) {
     teaserDate.textContent = current['teaserDate'];
     let teaserText = document.createElement("p");
     teaserText.innerHTML = current['teaserText'];
-    teaserText.style.backgroundColor = "rgb(4,88,136)";
+    teaserText.style.backgroundColor = "rgba(4,88,136,0.5)";
     teaserText.style.padding = "20px";
 
     update.appendChild(teaserTitle);
